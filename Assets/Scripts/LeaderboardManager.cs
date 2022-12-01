7d0ce113599b0a9c668bbecc86042c333d0db5ac
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2022-12-01 15:46:12 +0000

    Commit 4.1
    Removed a namespace which caused a compilation error

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Searcher;
 using UnityEngine.SceneManagement;
 
 public class LeaderboardManager : MonoBehaviour
